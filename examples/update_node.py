55b137af8c3d58c25ea8b5a5975ff6046e860000
Santiago Larrain <slarrain@uchicago.edu>
2017-06-05 14:54:31 -0400

    Undo SS's changes to the examples

--- examples/update_node.py
+++ examples/update_node.py
@@ -3,25 +3,22 @@ import json
 from dkan.client import DatasetAPI
 
 uri = os.environ.get('DKAN_URI', False)
-#user = os.environ.get('DKAN_USER', 'admin')
-#password = os.environ.get('DKAN_PASSWORD', 'admin')
-token = 'put your services token here'
+user = os.environ.get('DKAN_USER', 'admin')
+password = os.environ.get('DKAN_PASSWORD', 'admin')
 
 if uri:
-  #api = DatasetAPI(uri, user, password, True)
-  api = DatasetAPI(uri, token, True)
-  #data = {
-  #    'title': 'Test Dataset',
-  #    'type': 'dataset'
-  #}
-  #dataset = api.node('create', data=data)
-  #print dataset.status_code
-  #print dataset.text
-  #dataset = dataset.json()
+  api = DatasetAPI(uri, user, password, True)
+  data = {
+      'title': 'Test Dataset',
+      'type': 'dataset'
+  }
+  dataset = api.node('create', data=data)
+  print dataset.status_code
+  print dataset.text
+  dataset = dataset.json()
   update = {
     'title': 'Test Dataset Updated',
   }
-  #change the node_id to the dataset node id that you want to update, e.g. node_id=1041
   r = api.node('update', node_id=dataset['nid'], data=update)
   print r.status_code
   print r.text
